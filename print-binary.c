8873f208e8ae736ef7f7035e309651c2c6d22019
Shaun Rowland <rowland@corelight.com>
2017-08-16 00:35:14 -0400

    Fixed tabbing.

--- print-binary.c
+++ print-binary.c
@@ -36,7 +36,7 @@ main(void)
 {
     char input[MAX_LINE];
     uint32_t number, old_number;
-	int x, n;
+    int x, n;
 
     printf(PROMPT);
 
@@ -51,14 +51,14 @@ main(void)
         printf("\nHost Order:\t\t\t%#010x\n", number);
 
         // This is meant to show each byte as hexidecimal in the order the
-		// bytes are stored in RAM. This is mostly interesting on a little
-		// endian system.
-    	printf("RAM Bytes in Order:\t\t");
+        // bytes are stored in RAM. This is mostly interesting on a little
+        // endian system.
+        printf("RAM Bytes in Order:\t\t");
 
-    	for (n = 0; n < 4; n++) {
-        	x = (number >> (8 * n)) & 0xff;
-        	printf("%#04x ", x);
-    	}
+        for (n = 0; n < 4; n++) {
+            x = (number >> (8 * n)) & 0xff;
+            printf("%#04x ", x);
+        }
 
         printf("\n\n");
         number = old_number;
